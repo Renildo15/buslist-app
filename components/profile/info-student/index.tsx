57be4e60e6f18c771230591930474cfd79027bba
Renildo15 <renildorabi@ufrn.edu.br>
2024-10-29 19:14:45 -0300

    feat: fazendo upload de imagem

--- components/profile/info-student/index.tsx
+++ components/profile/info-student/index.tsx
@@ -3,15 +3,83 @@ import { View, Text } from '@/components/Themed';
 import { styles } from './styles';
 import { useSession } from '@/context/AuthContext';
 import { getTeachingLevel } from '@/utils';
+import { TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+import { uploadAvatar, useWhoAmI } from '@/api/api';
+import Toast from 'react-native-toast-message';
+import TextInfo from './text-info';
+import ButtonAvatarSave from './button-avatar';
 
 export default function InfoStudent() {
-  const { user } = useSession();
+  const { user, session } = useSession();
+  const [avatarUri, setAvatarUri] = useState<string | null>(user?.profile.avatar ?? null);
+  const [showButtonAvatar, setShowButtonAvatar] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const { mutate: userInfoMutate} = useWhoAmI(session ?? '');
+
+  const handleSelectImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    })
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setAvatarUri(result.assets[0].uri);
+      setShowButtonAvatar(true);
+    }
+  }
+
+  const handleUploadAvatar = async () => {
+    const data = new FormData();
+    const response = await fetch(avatarUri ?? '');
+    const blob = await response.blob();
+   //@ts-ignore
+    data.append('avatar', {
+      uri: avatarUri,
+      name: 'avatar.jpg',
+      type: blob.type,
+    });
+  
+    try {
+      setIsLoading(true);
+      await uploadAvatar(session ?? '', data);
+      userInfoMutate();
+      setShowButtonAvatar(false);
+  
+      Toast.show({
+        type: 'success',
+        text1: 'Avatar atualizado',
+        text2: 'Seu avatar foi atualizado com sucesso',
+      });
+    } catch (error) {
+      console.error('Error uploading avatar:', error);
+  
+      Toast.show({
+        type: 'error',
+        text1: 'Erro ao atualizar avatar',
+        text2: 'Ocorreu um erro ao atualizar seu avatar, tente novamente',
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  
+
   return (
     <View style={styles.container}>
       <Text style={styles.header_text}>Informações sobre o estudante</Text>
       <View>
         <View style={styles.avatar_container}>
-          <Avatar height={70} width={70} borderRadius={50} uri={user?.profile.avatar ?? ''} />
+          <TouchableOpacity style={styles.camera_icon} onPress={handleSelectImage}>
+            <Avatar height={70} width={70} borderRadius={50} uri={avatarUri ?? ''} />
+            <Feather name="camera" size={20} color="#007bff" style={styles.camera_icon_position} />
+          </TouchableOpacity>
           <View style={styles.student_info_box}>
             <Text>{user?.first_name} {user?.last_name}</Text>
             <Text
@@ -27,17 +95,7 @@ export default function InfoStudent() {
         <TextInfo label="Instituição:" value={user?.profile.institution ?? ''} />
         <TextInfo label="Parada:" value={user?.profile.bus_stop ?? 'Sem parada'} />
       </View>
-    </View>
-  );
-}
-
-function TextInfo({ label, value }: { label: string; value: string }) {
-  return (
-    <View style={{ flexDirection: 'row', gap: 8, alignItems: 'center' }}>
-      <Text style={{ fontSize: 14, fontWeight: 'bold' }}>{label}</Text>
-      <Text style={{ fontSize: 12, fontWeight: 'semibold', color: 'gray' }}>
-        {value}
-      </Text>
+      {showButtonAvatar && <ButtonAvatarSave onPress={handleUploadAvatar} isLoading={isLoading} />}
     </View>
   );
 }
--- components/profile/info-student/styles.ts
+++ components/profile/info-student/styles.ts
@@ -8,4 +8,22 @@ export const styles = StyleSheet.create({
   header_text: { fontSize: 18, fontWeight: 'bold' },
   avatar_container: { flexDirection: 'row', alignItems: 'center', gap: 8 },
   student_info_box: { flexDirection: 'column' },
+  camera_icon: {
+    position: 'relative',
+  },
+  camera_icon_position: {
+    position: 'absolute',
+    right: 5,
+    bottom: 5,
+  },
+  box_update_avatar: {
+    justifyContent:'center', 
+    alignItems:'center',
+    marginTop:10
+  },
+  button_update_avatar: {
+    padding:10, 
+    borderRadius:5, 
+    backgroundColor:'#007bff'
+  }
 });
