d9be2f38fd2596a374917aeee0375b0276fa689d
Renildo15 <renildorabi@ufrn.edu.br>
2024-10-29 19:13:29 -0300

    feat: criando compoinente TextInfo

---
+++ components/profile/info-student/text-info/index.tsx
@@ -0,0 +1,17 @@
+import { View,Text } from "@/components/Themed";
+
+interface TextInfoProps {
+    label: string;
+    value: string;
+}
+
+export default function TextInfo({ label, value }: TextInfoProps) {
+    return (
+      <View style={{ flexDirection: 'row', gap: 8, alignItems: 'center' }}>
+        <Text style={{ fontSize: 14, fontWeight: 'bold' }}>{label}</Text>
+        <Text style={{ fontSize: 12, fontWeight: 'semibold', color: 'gray' }}>
+          {value}
+        </Text>
+      </View>
+    );
+  }
