c52cbdd3a790be48bdf6a6d5196f8c777d214579
Renildo15 <renildorabi@ufrn.edu.br>
2024-11-03 10:29:00 -0300

    feat:criando modal

---
+++ components/notice/modal-notice/index.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { INotice } from '@/api/interfaces/notice';
+import {Alert, Modal, StyleSheet, Text, Pressable, View} from 'react-native';
+import { styles } from './styles';
+import { Feather } from '@expo/vector-icons';
+import { useSession } from '@/context/AuthContext';
+import { updateViewed, useNotices } from '@/api/api';
+
+interface IModalNoticeProps {
+    modalVisible: boolean;
+    setModalVisible: (modalVisible: boolean) => void;
+    notice: INotice;
+}
+
+export default function ModalNotice({notice, modalVisible, setModalVisible}: IModalNoticeProps) {
+
+  const { session } = useSession();
+  const { mutate: mutateNotices } = useNotices( session ?? null);
+
+  const handleViewed = async () => {
+    try {
+        await updateViewed(session ?? null, notice.id);
+        await mutateNotices();
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+  return (
+    <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+        Alert.alert('Modal has been closed.');
+        setModalVisible(!modalVisible);
+    }}>
+        <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+                <View style={{ backgroundColor: 'transparent' }}>
+                    <Feather name="alert-triangle" size={50} color="#007bff" />
+                </View>
+                <Text style={styles.modalText}>{notice.title}</Text>
+                <Text>{notice.description}</Text>
+                <View style={{width:'100%', marginTop:70}}>
+                    <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => {
+                            if (!notice.viewed) {
+                                handleViewed();
+                            }
+                            setModalVisible(!modalVisible);
+                        }}
+                    >
+                        {notice && !notice.viewed ? <Text style={styles.textStyle}>OK</Text> : <Text style={styles.textStyle}>Fechar</Text>}
+                    </Pressable>
+                 </View>
+            </View>
+        </View>
+    </Modal>
+  );
+};
+
---
+++ components/notice/modal-notice/styles.ts
@@ -0,0 +1,55 @@
+import { StyleSheet } from 'react-native';
+
+
+export const styles = StyleSheet.create({
+    centeredView: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'rgba(0,0,0,0.5)',
+    },
+    modalView: {
+      margin: 20,
+      backgroundColor: 'white',
+      borderRadius: 20,
+      padding: 35,
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5,
+      width: '80%',
+      height: '40%',
+      
+    },
+    button: {
+      borderRadius: 20,
+      padding: 10,
+      elevation: 2,
+    },
+    buttonOpen: {
+      backgroundColor: '#F194FF',
+    },
+    buttonClose: {
+      backgroundColor: '#2196F3',
+      width: '100%',
+    },
+    textStyle: {
+      color: 'white',
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+    modalText: {
+      marginBottom: 15,
+      textAlign: 'center',
+      fontWeight: 'bold',
+      fontSize: 20,
+    },
+  });
+  
+  
