09326c52a80080e23e6daea32a7ffedb2e11d828
Renildo15 <renildorabi@ufrn.edu.br>
2024-11-09 11:29:00 -0300

    feat: conmponente button

---
+++ components/button/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { TouchableOpacity, TouchableOpacityProps, Text, StyleSheet, DimensionValue } from "react-native";
+
+interface IButtonProps extends TouchableOpacityProps {
+    title: string;
+    color?: string;
+    width?: string;
+}
+ 
+export function Button(props: IButtonProps){
+    return (
+        <TouchableOpacity {...props} style={[{backgroundColor:props.color, width:props.width as DimensionValue}, styles.button]}>
+            <Text style={styles.button_text}>{props.title}</Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        padding: 10,
+        borderRadius: 5,
+        alignItems: 'center',
+    },
+    button_text: {
+        color: 'white',
+    },
+});
