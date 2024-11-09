e694474966bf039e20b6cb76db778acaec187475
Renildo15 <renildorabi@ufrn.edu.br>
2024-11-09 08:40:30 -0300

    feat: criando button Icon

---
+++ components/button-icon/index.tsx
@@ -0,0 +1,16 @@
+import Feather from "@expo/vector-icons/Feather";
+import { TouchableOpacity } from "react-native";
+
+interface IButtonIconProps {
+    name: keyof typeof Feather.glyphMap;
+    size: number;
+    onPress?: () => void;
+}
+
+export default function ButtonIcon({ name, size, onPress }: IButtonIconProps) {
+    return (
+        <TouchableOpacity onPress={onPress} activeOpacity={0.8}>
+            <Feather name={name} size={size} color="#007bff" />
+        </TouchableOpacity>
+    );
+}
